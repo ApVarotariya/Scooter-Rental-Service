b7e171694813061c7c10ef4b4d85b1918ff08582
Amit Varotariya <apvarotariya17@gmail.com>
2022-02-15 10:20:04 +0530

    js commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 burger = document.querySelector(".burger")
 navbar = document.querySelector(".navbar")
 logo = document.querySelector(".logo")
-rightNav = document.querySelector(".right-nav")
+rightnav = document.querySelector(".rightnav")
 
 burger.addEventListener("click", () => {
-  rightNav.classList.toggle("v-class-resp")
-  logo.classList.toggle("v-class-resp")
+  rightnav.classList.toggle("v-class-resp")
+  // logo.classList.toggle("v-class-resp")
   navbar.classList.toggle("h-nav-resp")
 })
